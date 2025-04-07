f313728a19cc6e46e42bb20f09e97c770890e0a1
srujan5565 <srujanvaddepally16@gmail.com>
2025-04-07 23:48:34 +0530

    Recap OOPs

---
+++ OOPsConceptPrograms/AverageOOPs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Average{
+    int sum=0,nov=0;
+    Scanner sc=new Scanner(System.in);
+    public void start(){
+        while(true){
+            int num = sc.nextInt();
+            if(num == 0) break;
+            sum +=num;
+            nov++;
+        }
+        System.err.println((float)sum/nov);
+    }
+}
+public class AverageOOPs {
+    public static void main(String[] args) {
+        Average avg = new Average();
+        avg.start();
+    }
+}
