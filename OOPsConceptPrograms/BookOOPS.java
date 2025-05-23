c4e3e32abab59f10c448b3a8e0316af598940daf
Srujan Vaddepally <srujanvaddepally16@gmail.com>
2025-05-23 23:39:57 +0530

    InfosysD1

--- OOPsConceptPrograms/BookOOPS.java
+++ OOPsConceptPrograms/BookOOPS.java
@@ -1,3 +1,35 @@
+import java.util.Objects;
+
+class Book {
+    private String title;
+    private int year;
+    private String description;
+
+    public Book(String title, int year, String description) {
+        this.title = title;
+        this.year = year;
+        this.description = description;
+    }
+
+    // Override equals() to check content equality
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;  // same reference
+        if (obj == null || getClass() != obj.getClass()) return false;  // type check
+
+        Book book = (Book) obj;
+        return year == book.year &&
+               Objects.equals(title, book.title) &&
+               Objects.equals(description, book.description);
+    }
+
+    // Optional: Override hashCode() if you override equals
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, year, description);
+    }
+}
+
 public class BookOOPS {
     public static void main(String[] args) {
         Book bookObject = new Book("Book object", 2000, "...");
